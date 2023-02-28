0822ef9a80987831ecede2b66addb4333094c62f
rafiimamibrahim <118794949+rafiimamibrahim@users.noreply.github.com>
2023-02-27 20:55:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+// //let nilai = prompt("masukan nilai : ", 0);
+// //let hasil = "";
+
+// //switch(true){
+//     //case nilai >= 91 && nilai <= 100 :
+//         //hasil= "A"
+//         //break;
+
+//     //case nilai  >= 81 && nilai <= 90 :
+//         //hasil= "B"
+//         //break;
+            
+//     //case nilai  >= 71 && nilai <= 80 :
+//         //hasil= "C"
+//         //break;
+
+//     //case nilai  >= 61 && nilai <= 70 :
+//         //hasil= "D"
+//         //break;
+        
+//     //case nilai < 60 :
+//         //hasil= "Maaf Kamu Mendapatkan Nilai F"
+//         //break;
+//         //default:
+//             //document.write("Maaf nilai anda berlebihan");
+
+// //}
+//  //document.write(hasil);
+
+//  let user = prompt("Username : ", "");
+// let pass = prompt("Password : ", "");
+
+// if(user.toUpperCase() == "IBRA"){
+//     if(pass == "ibra26"){
+//         document.write(`<h1>Selamat datang ${user}`);
+    
+//     }else{
+//     document.write("<p>Password salah, silahkan coba lagi</p>");
+//     }
+// }else{
+
+
+// }
+
+// if(user.toUpperCase() == "MARSHA"){
+//     if(pass == "MARSHA"){
+//         document.write(`<h1>Selamat datang ${user}`);
+    
+//     }else{
+//     document.write("<p>Password salah, silahkan coba lagi</p>");
+//     }
+// }else{
+
+
+
+
+// }
+
+// if(user.toUpperCase() == "SHABRINA"){
+//     if(pass == "AURORA"){
+//         document.write(`<h1>Selamat datang ${user}`);
+    
+//     }else{
+//     document.write("<p>Password salah, silahkan coba lagi</p>");
+//     }
+// }else{
+
+// document.write("<p> Anda tidak terdaftar!</p>");
+
+// }
+
+var phi;
+var jari2;
+var total;
+
+function jumlahLuasLingkaran(a, b){
+    jariJari = a ** 2;
+    pi = 3.14
+    total = pi * jariJari;
+    return total;
+}
+
+alert(jumlahLuasLingkaran(10))
